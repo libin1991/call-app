3d81fe815abc091375aeb803c2b57f7fa189ba24
libin <li.bin_201818@immomo.com>
2021-12-15 12:51:59 +0800

    perf: sw code

--- rollup.config.js
+++ rollup.config.js
@@ -2,31 +2,35 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
-
-const input = 'src/index.ts';
+import path from 'path';
+import ts from "rollup-plugin-ts"; // 支持sw TS文件
 const extensions = ['.js', '.ts'];
-
+const getPath = _path => path.resolve(__dirname, _path);
 export default {
-  input,
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-    {
-      file: pkg.browser,
-      format: 'umd',
-      name: 'CallApp',
-    },
+  input: getPath('./src/index.ts'),
+  output: [{
+    file: pkg.module,
+    format: 'es',
+  },
+  {
+    file: pkg.main,
+    format: 'cjs',
+  },
+  {
+    file: pkg.browser,
+    format: 'umd',
+    name: 'CallApp',
+  },
   ],
   external: [],
   plugins: [
     resolve({ extensions }),
     commonjs(),
+    ts({
+      tsconfig: {
+        allowJs: true
+      }
+    }),
     babel({
       extensions,
       include: ['src/**/*'],
